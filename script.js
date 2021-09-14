7be99656b1d5747eeb4d133943b0547b5d58a92d
Blantoke <86419796+Blantoke@users.noreply.github.com>
2021-09-14 03:22:13 -0400

    Update script.js
    Adding in JS for page 1

---
+++ script.js
@@ -0,0 +1,13 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var button = document.getElementById("autodraft");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+button.onclick = function () {
+  location.href="pagetwo.html"
+}
