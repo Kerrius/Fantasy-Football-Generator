b03a6f92787aaa1975bc8da76f3dc709a99b0ecb
Kerrius <84813785+Kerrius@users.noreply.github.com>
2021-09-14 18:39:18 -0400

    added js

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-var button = document.getElementById("autodraft");
-output.innerHTML = slider.value; // Display the default slider value
+//link api's
+  fetch('https://www.fantasyfootballdatapros.com/api/projections')
+  .then(response => response.json())
+    //.then(data => console.log(data));
+    .then(data => {
+      var playerInfoArray = data ['projections'];
+  
+    })
+    
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+
+
+
+addEventListener("click", function () {
+  myTeam = startDraftButtonClicked();
+  document.getElementById("startdraft") = myTeam;
+});
+
+var playerInfoArray = [];
+var teamPicksArray = []; // Maps the team number to an overall pick for both linear and snake formats
+var overallPick = 1; // Used to index the teamPicksArray
+var MAX_QB = 3;
+var MAX_RB = 5;
+var MAX_WR = 5;
+var MAX_TE = 2;
+var MAX_DST = 2;
+var MAX_K = 2;
+var numRosterSpots = 12;
+var numRounds = 7;
+var myTeam = [];
+var myRange = document.getElementById('myRange')
+
+
+// Constructor for PlayerInfo object
+class PlayerInfo {
+  constructor(rank, playerTeam, position, bye, projectedPoints) {
+    this.rank = rank;
+    this.playerTeam = playerTeam;
+    this.position = position;
+    this.bye = bye;
+    this.projectedPoints = projectedPoints;
+    this.fantasyTeam = "";
+    this.round = 0;
+    this.pick = 0;
+    this.overallPick = 0;
+
+    this.toString = function () {
+      return this.rank + "," + this.playerTeam + "," + this.position + "," + this.bye + "," + this.projectedPoints;
+    };
+  }
 }
 
-button.onclick = function () {
-  location.href="pagetwo.html"
-}
 
+
+function startDraftButtonClicked()
+{    
+    var myRange = myRange;
+    var numRosterSpots = numRounds;
+    
+    
+    // Clear out parts of objects in playerInfoArray
+    for(var i = 0; i < playerInfoArray.length; i++)
+    {
+        playerInfoArray[i].fantasyTeam = "";
+        playerInfoArray[i].round = 0;
+        playerInfoArray[i].pick = 0;
+        playerInfoArray[i].overallPick = 0;
+    }
+}
